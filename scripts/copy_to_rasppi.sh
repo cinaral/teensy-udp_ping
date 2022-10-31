0782f43e5d1a324c7c93127d04d9b754919395d8
cinaral <alevent.cinar@gmail.com>
2022-10-31 15:40:51 -0500

    Update scripts

--- scripts/copy_to_rasppi.sh
+++ scripts/copy_to_rasppi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp .pio/build/teensy41/firmware.hex ./
-rsync -av --exclude='.*' --exclude='*.ini' --exclude='scripts/' ./ beryl@10.66.203.90:/home/beryl/teensy-udp_ping/
+rsync -av --exclude='.*' --exclude='*.ini' ./ beryl@10.66.203.90:/home/beryl/teensy-udp_ping/
 rm firmware.hex
